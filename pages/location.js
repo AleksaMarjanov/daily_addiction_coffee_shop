073d6d3eb004f6ce7f1765a778887f7bc5cd1221
Aleksa Marjanov <Aleksa.marjanov@outlook.com>
2022-09-07 23:54:26 +0000

    add map

--- pages/location.js
+++ pages/location.js
@@ -1,12 +1,20 @@
-import React from 'react'
-import AppWrap from '../src/wrapper/AppWrap'
-import MotionWrap from '../src/wrapper/MotionWrap'
+import React from "react";
+import AppWrap from "../src/wrapper/AppWrap";
+import MotionWrap from "../src/wrapper/MotionWrap";
+import dynamic from "next/dynamic";
 
+const Map = dynamic(() => import("../src/container/Map"), {
+  ssr: false,
+});
 
 const Location = () => {
   return (
-    <div id="location">locations</div>
-  )
-}
+      
+      <div className="relative z-[0] h-[40vh] w-[50%]">
+        <Map />
+      </div>
+  );
+};
 
-export default AppWrap(MotionWrap(Location, "location"))
+export default AppWrap(MotionWrap(Location, "location"));
+ 
