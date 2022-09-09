a3ab4e92a3b174a16f09fb95f90dfc78a9533805
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-09-09 16:03:50 -0500

    ..map

--- pages/location.js
+++ pages/location.js
@@ -9,12 +9,17 @@ const Map = dynamic(() => import("../src/container/Map"), {
 
 const Location = () => {
   return (
-      
-      <div className="relative z-[0] h-[40vh] w-[50%]">
-        <Map />
+    <>
+      <div className="w-full flex flex-col justify-center items-center mt-5">
+        <h2 className="font-poppins font-semibold text-4xl md:text-xl sm:text-sm ">
+        The Best Quality Coffee In Town For You!
+        </h2>
+        <div className="mt-6 md:mt-2 relative z-[0] h-[60vh] w-[60%] md:w-[75%] sm:w-[80%]">
+          <Map />
+        </div>
       </div>
+    </>
   );
 };
 
 export default AppWrap(MotionWrap(Location, "location"));
- 
