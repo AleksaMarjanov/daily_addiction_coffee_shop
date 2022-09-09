fce80ef48e041edd4dcdd8853a7cb4d9c93479f2
Aleksa Marjanov <Aleksa.marjanov@outlook.com>
2022-09-09 21:51:36 +0000

    add Contact

--- pages/contact.js
+++ pages/contact.js
@@ -1,12 +1,38 @@
-import React from 'react'
-import AppWrap from '../src/wrapper/AppWrap'
-import MotionWrap from '../src/wrapper/MotionWrap'
-
+import React, {useState} from "react";
+import AppWrap from "../src/wrapper/AppWrap";
+import MotionWrap from "../src/wrapper/MotionWrap";
 
 const Contact = () => {
-  return (
-    <div>contact</div>
-  )
-}
+  const [form, setForm] = useState({
+    fullName,
+    email,
+    message
+  })
 
-export default AppWrap(MotionWrap(Contact, "contact"))
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <h2 className="text-3xl font-semibold">Contact Us</h2>
+      <div className="flex flex-col mt-5 justify-center items-center border border-b-2 border-gray-300">
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="fullName" placeholder="Jane Doe" value={fullName} required />
+          <input
+            type="email"
+            name="email"
+            placeholder="janedoe@gmail.com"
+            value={email}
+            required
+          />
+          <button type="button" onClick={handleSubmit}>
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AppWrap(MotionWrap(Contact, "contact"));
