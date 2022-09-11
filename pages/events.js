74369e28bf34aa064e417a187b27a08893c90347
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-09-11 07:45:19 -0500

    ..

--- pages/events.js
+++ pages/events.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import AppWrap from '../src/wrapper/AppWrap'
 import MotionWrap from '../src/wrapper/MotionWrap'
+import Image from 'next/image'
 
 import { client, urlFor } from '../src/client'
 
@@ -21,11 +22,11 @@ const Events = () => {
       <p>Search...</p>
       <div>
         {events.map((event, index) => {
-          <div class="max-w-sm rounded overflow-hidden shadow-lg" key={event.name + index}>
-          <img class="w-full" src={`${urlFor(event.imgurl)}`} alt="Sunset in the mountains"/>
-          <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">{event.name}</div>
-            <p class="text-gray-700 text-base">
+          <div className="max-w-sm rounded overflow-hidden shadow-lg" key={event.name + index}>
+          <Image className="w-full" src={`${urlFor(event.imgurl)}`} alt="Sunset in the mountains" />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">{event.name}</div>
+            <p className="text-gray-700 text-base">
               {event.description}
             </p>
           </div>
