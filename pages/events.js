454e77f66ca0f15d51a32478f67650d2452cada8
Aleksa Marjanov <Aleksa.marjanov@outlook.com>
2022-09-12 01:15:41 +0000

    ..

--- pages/events.js
+++ pages/events.js
@@ -107,7 +107,7 @@ const Events = () => {
               </p>
               <a
                 href="#"
-                className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-coffee-blue rounded-lg hover:bg-secondary-color focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 Read more
                 <svg
