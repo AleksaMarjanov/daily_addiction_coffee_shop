d66c6e713544049aa27910907edb85c04a886a2e
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-09-16 18:08:29 -0500

    filter buttons

--- pages/menu.js
+++ pages/menu.js
@@ -39,9 +39,9 @@ const menus = () => {
 
 
 
-  const itemActive = 'xl:border-2 hover:bg-primary xl:border-coffee-blue px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-white';
+  const itemActive = 'xl:border-2 hover:bg-primary xl:border-coffee-blue px-3 py-2 rounded xl:rounded-lg flex items-center gap-2 justify-center cursor-pointer text-white';
 
-  const notActiveItem = 'xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black'
+  const notActiveItem = 'xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-lg flex items-center gap-2 justify-center cursor-pointer text-black'
 
   return (
     <div className="flex-1 w-full flex-col bg-gray-100 font-poppins ">
