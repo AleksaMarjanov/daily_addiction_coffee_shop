32175d146155214f400ed19e2108475028a6919c
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-09-15 19:01:30 -0500

    Filter active item in menu

--- pages/menu.js
+++ pages/menu.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useLayoutEffect } from "react";
 import AppWrap from "../src/wrapper/AppWrap";
 import MotionWrap from "../src/wrapper/MotionWrap";
 import {client, urlFor} from "../src/client";
@@ -9,18 +9,20 @@ const menus = () => {
   const [menus, setMenus] = useState([]);
   const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
   const [activeFilter, setActiveFilter] = useState("All");
-  const [filtermenus, setFiltermenus] = useState([]);
+  const [filterMenus, setFilterMenus] = useState([]);
 
   useEffect(() => {
     const query = '*[_type == "menu"]';
 
     client.fetch(query).then((data) => {
       setMenus(data);
-      setFiltermenus(data);
+      setFilterMenus(data);
     });
   }, []);
 
-  const handlemenusFilter = (item) => {
+
+
+  const handleMenusFilter = (item) => {
     setActiveFilter(item);
     setAnimateCard([{ y: 100, opacity: 0 }]);
 
@@ -28,24 +30,28 @@ const menus = () => {
       setAnimateCard([{ y: 0, opacity: 1 }]);
 
       if (item === "All") {
-        setMenus(menus);
+        setFilterMenus(menus);
       } else {
-        setMenus(menus.filter((menu) => menu.tags.includes(item)));
+        setFilterMenus(menus.filter((menu) => menu.tags.includes(item)));
       }
     }, 500);
   };
 
-  const itemActive = 'bg-coffee-blue text-white'
+
+
+  const itemActive = 'xl:border-2 hover:bg-primary xl:border-coffee-blue px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-white';
+
+  const notActiveItem = 'xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black'
 
   return (
-    <div className="flex-1 w-full flex-col">
-      <div className="flex flex-row justify-center items-center flex-wrap mt-32 mr-0 mb-16">
+    <div className="flex-1 w-full flex-col bg-gray-100 font-poppins ">
+      <div className="flex flex-row flex-wrap justify-center items-center mt-16 mr-0 mb-8">
         {["Breakfast", "Deserts", "Drinks", "All"].map((item, index) => (
           <div
             key={index}
-            onClick={() => handlemenusFilter(item)}
-            className={`pt-[0.5px] pr-4 bg-white rounded-lg text-black font-semibold cursor-pointer transition-all animate ease-in duration-300 hover:bg-coffee-blue hover:text-white flex justify-center items-center p-text ${
-              activeFilter === item ? `${itemActive}` : ""
+            onClick={() => handleMenusFilter(item)}
+            className={`pt-2 pr-4 m-2 bg-white rounded-lg text-black font-semibold cursor-pointer transition-all animate ease-in duration-300 hover:bg-coffee-blue hover:text-white flex justify-center items-center p-text ${
+              activeFilter === item ? `${itemActive}` : `${notActiveItem}`
             }`}
           >
             {item}
@@ -58,10 +64,10 @@ const menus = () => {
         transition={{ duration: 0.5, delayChildren: 0.5 }}
         className="flex flex-wrap justify-center items-center"
       >
-        {filtermenus.map((menu, index) => (
+        {filterMenus.map((menu, index) => (
           <div
-            className="w-270 flex-col m-8 p-4 rounded-lg bg-white text-black cursor-pointer transition-all animate duration-300 hover:shadow-xl flex justify-center items-center"
-            key={menu.name + index}
+            className="w-270 flex-col m-8 p-4 rounded-lg bg-white text-black cursor-pointer transition-all animate ease-in duration-300 hover:shadow-xl flex justify-center items-center"
+            key={index}
           >
             <div className="w-full h-230 relative flex justify-center items-center">
               <Image
@@ -71,6 +77,7 @@ const menus = () => {
                 className="w-full h-full rounded-lg object-cover"
                 width={300}
                 height={300}
+                priority
               />
 {/* 
               <motion.div
