f0bd0eb56b6a61336223979107c8e752a1518cb3
Aleksa Marjanov <Aleksa.marjanov@outlook.com>
2022-09-21 22:39:21 +0000

    add animation on navbar

--- pages/home.js
+++ pages/home.js
@@ -4,6 +4,7 @@ import MotionWrap from "../src/wrapper/MotionWrap";
 import Image from "next/image";
 import { images } from "../src/constants";
 import SEO from '@bradgarropy/next-seo'
+import Head from "next/head";
 
 import { client, urlFor } from "../src/client";
 import Link from "next/link";
@@ -53,33 +54,6 @@ const Home = () => {
          </Link>
         </figure>
       </div>
-      {/* <div>
-        <p className=" flex items-center justify-center text-xl p-5 mt-5">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.<br/> Architecto
-          delectus sit praesentium <br/>exercitationem aspernatur hic placeat ipsum
-          doloremque!<br/> Eum eveniet dolorem est unde itaque dolore sunt aut,
-          repellat ipsam quam.
-        </p>
-     
-      </div>
-      <div className="p-4 gap-8 sm:p-2 sm:gap-4 grid grid-flow-col grid-rows-4 md:flex md:flex-col sm:items-center sm:justify-center">
-      {pictures.map((picture, index) => (
-        <div
-          key={picture.name + index}
-          className={`flex items-center justify-between rounded-lg shadow-xl`}
-        >
-          <Image
-            src={`${urlFor(picture.imgurl)}`}
-            alt="gallery"
-            objectFit="cover"
-            width={300}
-            height={300}
-            loading="lazy"
-            className="object-center object-cover rounded-xl"
-            />
-        </div>
-      ))}
-      </div> */}
     </div>
 
     </>
