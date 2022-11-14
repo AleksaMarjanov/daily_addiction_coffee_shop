2bac7f84d8aa3e510b0c210ee98c5853818d0f35
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-11-14 17:16:45 -0600

    grid on about page

--- pages/about.js
+++ pages/about.js
@@ -31,7 +31,7 @@ const About = () => {
             <br /> in the heart of Downtown Williston, ND.
           </span>
         </section>
-        <figure className="mt-5 grid gap-6 grid-rows-3 grid-flow-col lg:grid-rows-2 minsm:grid-rows-4 sm:flex sm:flex-col max-w-6xl sm:max-w-lg minmd:max-w-xl cursor-pointer">
+        <figure className="mt-5 grid gap-6 grid-rows-3 grid-flow-col lg:grid-rows-2 minsm:grid-rows-4 sm:flex sm:flex-col max-w-6xl sm:max-w-lg minmd:grid-rows-4 minmd:max-w-4xl cursor-pointer">
           {gallery?.map((images, index) => (
             <div key={images.name + index}>
               <div className="shadow-xl items-center justify-center flex object-fit object-contain">
