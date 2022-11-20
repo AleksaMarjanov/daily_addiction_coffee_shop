e8a100eb337837e37f9a1428f8da5bcc6a7388fb
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-11-20 11:01:30 -0600

    ..

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   javascript: {
     ignoreBuildErrors: true,
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
